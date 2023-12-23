3b6070279ccef51e1a11bd585fa1b779fc4f99d0
Reza <Reza@DESKTOP-CKL538G>
2023-12-23 15:47:36 +0330

    Initiated and tested

---
+++ Hangfire/HangfireDemo/Controllers/JobController.cs
@@ -0,0 +1,44 @@
+﻿using Hangfire;
+using HangfireDemo.Jobs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangfireDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult CreateRecurringJob()
+        {
+            RecurringJob.AddOrUpdate("RecurringJob1",
+                () => Console.WriteLine("Recurring Job Triggered"),
+                "* * * * *");
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult CreateScheduledJob()
+        {
+            var scheduledDateTime = DateTime.UtcNow.AddSeconds(5);
+            var dateTimeOffset = new DateTimeOffset(scheduledDateTime);
+
+            var job1 = BackgroundJob.Schedule(() => Console.WriteLine("Hi 2"), dateTimeOffset);
+            var job2 = BackgroundJob.ContinueJobWith(job1, () => Console.WriteLine("Hi - continue 1"));
+            var job3 = BackgroundJob.ContinueJobWith(job2, () => Console.WriteLine("Hi - continue 2"));
+            BackgroundJob.ContinueJobWith(job3, () => Console.WriteLine("Hi - continue 3"));
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult CreateBackgroundJob()
+        {
+            //BackgroundJob.Enqueue(() => Console.WriteLine("Hey"));
+            BackgroundJob.Enqueue<TestJob>(i => i.WriteLog("Background JOb Triggered"));
+
+            return Ok();
+        }
+    }
+}
